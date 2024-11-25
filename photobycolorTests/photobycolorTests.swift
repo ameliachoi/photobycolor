0a22b3d2c89c70996f3b5810ddfa9dffa6ddbeab
AmeliaChoi <choijin9561@gmail.com>
2024-11-25 16:54:55 +0900

    Initial Commit

---
+++ photobycolorTests/photobycolorTests.swift
@@ -0,0 +1,17 @@
+//
+//  photobycolorTests.swift
+//  photobycolorTests
+//
+//  Created by Jin Choi on 11/25/24.
+//
+
+import Testing
+@testable import photobycolor
+
+struct photobycolorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
